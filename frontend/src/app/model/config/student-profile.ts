984f37b4dae7d7bf6165180e6b5b5282e3682803
sajjad <hsajjad490@gmail.com>
2023-11-05 12:46:22 +0600

    email field added in student-profile-form.component.html and model

--- frontend/src/app/model/config/student-profile.ts
+++ frontend/src/app/model/config/student-profile.ts
@@ -6,6 +6,7 @@ export interface StudentProfile extends AuditAbleModel {
     lastName?: string;
     contactNumber?: string;
     academicHistory?: string;
+    email?: string;
     // Assuming you might want to represent the related user in some way in the frontend:
     // userId?: number; // Or another User interface/type if you have it.
 }
